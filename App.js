9678182edaf9ff33d0badafd5915264f76338327
reemnu <reemnu@gmail.com>
2023-05-31 19:40:45 +0300

    :art: nav screens is working

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import Home from './screens/home';
 import SavedSets from './screens/savedSets'
+import Outfit from './screens/outfit'
 
 
 const Stack = createStackNavigator();
@@ -13,6 +14,7 @@ export default function App() {
     <Stack.Navigator>
       <Stack.Screen name='Home' component={Home}/> 
       <Stack.Screen name='SavedSets' component={SavedSets}/>
+      <Stack.Screen name='Outfit' component={Outfit}/>
     </Stack.Navigator>
     </NavigationContainer>
     );
--- screens/home.js
+++ screens/home.js
@@ -3,24 +3,46 @@ import { StyleSheet, View, Text,TouchableOpacity, Image, Alert} from 'react-nati
 import { globalStyles } from '../styles/global';
 
 
+
+
+
+
 export default function Home({ navigation }) {
 
   const pressHandler = () => {
     navigation.navigate('SavedSets');
   }
+  const shirtImagePress = () => {
+    navigation.navigate('Outfit');
+  };
+  const pantsImagePress = () => {
+    navigation.navigate('Outfit');
+  };
+  const shoesImagePress = () => {
+    navigation.navigate('Outfit');
+  };
+  
+  
+
 
   return (
     <View style={globalStyles.container}>
     <Text style={globalStyles.titleText}>Pick a outfit</Text>
     <CustomButton title="Saved Outfits" onPress={pressHandler} />
     <View style={styles.imageStyle}>
+      <TouchableOpacity onPress={shirtImagePress}>
       <Image source={require('../assets/shirt.png')} style={styles.imageShirt} />
+      </TouchableOpacity>
     </View>
     <View style={styles.imageStyle}>
+    <TouchableOpacity onPress={pantsImagePress}>
       <Image source={require('../assets/pants.png')} style={styles.imagePants} />
+      </TouchableOpacity>
     </View>
     <View style={styles.imageStyle}>
+    <TouchableOpacity onPress={shoesImagePress}>
       <Image source={require('../assets/shoes.png')} style={styles.imageShoes} />
+      </TouchableOpacity>
     </View>
       </View>
   );
@@ -33,6 +55,7 @@ const CustomButton = ({ title, onPress }) => {
     </TouchableOpacity>
   );
 };
+
   
 
 const styles = StyleSheet.create({
